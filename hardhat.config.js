851d8f8808ee90a8ab1ea22303c85d5b7130d8cc
Emiliano Scavuzzo <emiacc@hotmail.com>
2023-08-11 12:41:12 -0300

    Add gas reporter and optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,15 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version:  "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000
+      }
+    }
+  },
   networks: {
     hardhat: {
       chainId: 31337
@@ -20,5 +28,10 @@ module.exports = {
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+    coinmarketcap: process.env.COIN_MARKETCAP_API_KEY || "",
   }
 };
